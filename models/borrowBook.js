dca17f472597214744eed919d5c5fbc41ac3c79a
QuyenNguyen <quyennguyenforproject@gmail.com>
2025-05-31 11:48:00 +0700

    Schema for Borrow Book in a Borrow Session

---
+++ models/borrowBook.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const borrowBookSchema = new Schema({
+    book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
+    borrowSession: { type: Schema.Types.ObjectId, ref: 'BorrowSession', required: true },
+    returnDate: { type: Date, default: null },
+    overdueDay: { type: Number, default: 0 },
+    fineAmount: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model('BorrowBook', borrowBookSchema);
