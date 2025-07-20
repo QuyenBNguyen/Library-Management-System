4ee127961607bd91bc48d2176e0a344e328201f6
ducse <ducnsde160488@fpt.edu.vn>
2025-07-20 14:20:09 +0700

    fix bug

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const paymentController = require("../controllers/paymentController");
 const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
 
 router.get(
   "/",
